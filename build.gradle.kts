19a5519f08dca9eafa3ae7e8f9f70baa7aa4d51b
Siro_256 <siro@siro256.dev>
2023-08-20 22:09:18 +0900

    Fix unsuitable syntax

--- build.gradle.kts
+++ build.gradle.kts
@@ -52,69 +52,69 @@ tasks {
 
         from(sourceSets.main.get().allSource)
     }
+}
 
-    publishing {
-        publications {
-            create<MavenPublication>("publication") {
-                groupId = project.group.toString()
-                artifactId = project.name
-                version = project.version.toString()
+publishing {
+    publications {
+        create<MavenPublication>("publication") {
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
 
-                from(project.components.getByName("java"))
-                artifact(project.tasks.getByName("sourcesJar"))
+            from(project.components.getByName("java"))
+            artifact(project.tasks.getByName("sourcesJar"))
 
-                pom {
-                    name.set(artifactId)
-                    description.set(project.description)
+            pom {
+                name.set(artifactId)
+                description.set(project.description)
+                url.set("https://github.com/Kotatsu-RTM/KotatsuModelFormat-JVM")
+
+                licenses {
+                    license {
+                        name.set("The MIT License")
+                        url.set("https://opensource.org/license/mit/")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("Siro256")
+                        name.set("Siro_256")
+                        email.set("siro@siro256.dev")
+                        url.set("https://github.com/Siro256")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git://github.com/Kotatsu-RTM/KotatsuModelFormat-JVM.git")
+                    developerConnection.set("scm:git:ssh://github.com/Kotatsu-RTM/KotatsuModelFormat-JVM.git")
                     url.set("https://github.com/Kotatsu-RTM/KotatsuModelFormat-JVM")
-
-                    licenses {
-                        license {
-                            name.set("The MIT License")
-                            url.set("https://opensource.org/license/mit/")
-                        }
-                    }
-
-                    developers {
-                        developer {
-                            id.set("Siro256")
-                            name.set("Siro_256")
-                            email.set("siro@siro256.dev")
-                            url.set("https://github.com/Siro256")
-                        }
-                    }
-
-                    scm {
-                        connection.set("scm:git:git://github.com/Kotatsu-RTM/KotatsuModelFormat-JVM.git")
-                        developerConnection.set("scm:git:ssh://github.com/Kotatsu-RTM/KotatsuModelFormat-JVM.git")
-                        url.set("https://github.com/Kotatsu-RTM/KotatsuModelFormat-JVM")
-                    }
-                }
-            }
-        }
-
-        repositories {
-            maven {
-                url =
-                    if (version.toString().endsWith("SNAPSHOT")) {
-                        uri("https://repo.siro256.dev/repository/maven-snapshots")
-                    } else {
-                        uri("https://repo.siro256.dev/repository/maven-public")
-                    }
-                credentials {
-                    username = System.getenv("RepositoryUsername")
-                    password = System.getenv("RepositoryPassword")
                 }
             }
         }
     }
 
-    signing {
-        useInMemoryPgpKeys(
-            System.getenv("SigningKeyId"),
-            System.getenv("SigningKey"),
-            System.getenv("SigningKeyPassword")
-        )
-        sign(publishing.publications.getByName("publication"))
+    repositories {
+        maven {
+            url =
+                if (version.toString().endsWith("SNAPSHOT")) {
+                    uri("https://repo.siro256.dev/repository/maven-snapshots")
+                } else {
+                    uri("https://repo.siro256.dev/repository/maven-public")
+                }
+            credentials {
+                username = System.getenv("RepositoryUsername")
+                password = System.getenv("RepositoryPassword")
+            }
+        }
     }
+}
+
+signing {
+    useInMemoryPgpKeys(
+        System.getenv("SigningKeyId"),
+        System.getenv("SigningKey"),
+        System.getenv("SigningKeyPassword")
+    )
+    sign(publishing.publications.getByName("publication"))
 }
